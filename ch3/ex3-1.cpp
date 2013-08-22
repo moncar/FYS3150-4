7b200b992d5f59363b7d8cbe51c92a9aaeab3bfb
Marius Berge Eide <mariusei@astro.uio.no>
2013-08-22 16:06:56 +0200

    Passing test function as argument

--- ch3/ex3-1.cpp
+++ ch3/ex3-1.cpp
@@ -6,18 +6,28 @@
 
 using namespace std;
 
-double fFirst_deriv(double *fFuncIn(double fXvar) )
+double fFirstDeriv(double fX, double (*fFuncIn)(double) )
 {
-   return 0;
+   double fRes = (*fFuncIn)(fX);
+   fRes *= 3E-06;
+   cout << fRes << endl;
+   return fRes;
 }
 
 double fTestFunc(double fX)
 {
-   return pow(fX,3);
+   float fOut;
+   fOut = (fX + 3)*2.0;
+   cout << fOut << endl;
+   return fOut;
 }
 
 int main() 
 {
-   double fRes1 = fFirst_deriv(fTestFunc(2.0));
+   //double fRes1 = fFirst_deriv(fTestFunc(2.0));
+   double fRes1;
+   fRes1 = fFirstDeriv(2.0, fTestFunc);
+   //fRes1 = fFirstDeriv(fTestFunc(2.0));
+   cout << fRes1 << endl;
    return 0;
 }
