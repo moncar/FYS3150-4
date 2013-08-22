a86505c4f9043aa01d2c6d30df0289afa0779ba2
Marius Berge Eide <mariusei@astro.uio.no>
2013-08-22 15:32:40 +0200

    Added empty derivative function, test function and function call in main

--- ch3/ex3-1.cpp
+++ ch3/ex3-1.cpp
@@ -6,6 +6,18 @@
 
 using namespace std;
 
-main() 
+double fFirst_deriv(double *fFuncIn(double fXvar) )
 {
+   return 0;
+}
+
+double fTestFunc(double fX)
+{
+   return pow(fX,3);
+}
+
+int main() 
+{
+   double fRes1 = fFirst_deriv(fTestFunc(2.0));
+   return 0;
 }
