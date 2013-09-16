04a4f05eab3205e39a71241b4b7ce39a0f80ede4
Marius Berge Eide <mariusbe@gmail.com>
2013-09-16 05:15:38 +0200

    Holding references to different files - CORRECT????

--- oblig1/diffsolver.h
+++ oblig1/diffsolver.h
@@ -1,4 +1,9 @@
 #ifndef fSolver_H
 #define fSolver_H
 void solver(double *fX, double fH, double N);
+
+void LUmethod(int N, double *fSrcX);
+
+void outputFile(int N, double *fX, double *fCalcX, char *filename[]);
+
 #endif
