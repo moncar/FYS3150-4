2fce575926f6a868a559f25346b82a33af703caf
Marius Berge Eide <mariusbe@gmail.com>
2013-09-16 05:15:58 +0200

    Iteration prefixes are tuned and a timer is implemented

--- oblig1/functions.cpp
+++ oblig1/functions.cpp
@@ -4,30 +4,35 @@
 //  1) iterating forward, applying row multiplication factors (pivoting)
 //  2) iterating backward, applying row m. factors (back substitution)
 //  3) iterating forward, applying normalisation factor (gives leading 1s)
-
 #include <iostream>
-#include <math.h>
-using namespace std;
+#include "time.h"
+
 void solver(double *fX, double fH, double N)
 {
-    double j = 0;
+    //begin timer
+    clock_t start2, finish2;
+    start2 = clock();
+    
     
     // 1) Forward: 
     for (int iii=1; iii<N; iii++) {
-        j = iii + 1.0; 
-        fX[iii] += (j-1)/j * fX[iii-1];
+        fX[iii] += iii/(iii + 1.0)  * fX[iii-1];
     }
     
     // 2) Backward:
     for (int jjj=N-2; jjj>= 0; jjj--) {
-        j = jjj+1;
-        fX[jjj] += (j+1)/(j+2) * fX[jjj+1];
+        fX[jjj] += (jjj + 1.0)/(jjj+2.0) * fX[jjj+1];
     }
 
     // 3) Normalise:
     for (int kkk=0; kkk<N; kkk++) {
-        j = kkk+1;
-        fX[kkk] = pow(fH,2) *  j/(j+2) * fX[kkk];
+        fX[kkk] *= (kkk+1.0)/(kkk+2.0);
     }
 
+
+    // stop timer
+    finish2 = clock();
+
+    std::cout << "Time elapsed: tridiagonal solution scheme:" << std::endl;
+    std::cout << ((finish2 - start2) / CLOCKS_PER_SEC ) << std::endl;
 }
