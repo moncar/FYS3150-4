755e78f7db13d4a37d0952a8028ee3bc9f4fe0be
Marius Berge Eide <mariusbe@gmail.com>
2013-10-02 11:23:19 +0200

    Updated with different eigenvalue finders

--- oblig2/headers.h
+++ oblig2/headers.h
@@ -4,3 +4,24 @@
 void jacobiRotation(arma::mat &A, int N);
 
 #endif
+
+#ifndef VSTARTJACOBI_H
+#define VSTARTJACOBI_H
+
+void vStartJacobi(double N, double fRho_min, double fRho_max);
+
+#endif
+
+#ifndef VSTARTEIGVAL_H
+#define VSTARTEIGVAL_H
+
+void vStartEIGVAL(double N, double fRho_min, double fRho_max);
+
+#endif
+
+#ifndef VSTARTTQLI_H 
+#define VSTARTTQLI_H 
+
+void vStartTQLI(int N, double fRho_min, double fRho_max);
+
+#endif
