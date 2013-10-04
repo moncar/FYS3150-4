f3fb51b52689a4b31cd440ccaf8b0ba82f2c35cd
Marius Berge Eide <mariusbe@gmail.com>
2013-10-04 15:02:33 +0200

    Plots wave functions with correct axes labels

--- oblig2/plotter.py
+++ oblig2/plotter.py
@@ -11,7 +11,7 @@ try:
 #    filename2 = sys.argv[2]
 #    filename3 = sys.argv[3]
 except IndexError:
-    filename1 = 'res10.txt'
+    filename1 = 'resnew2_vec.txt'
 #    filename2 = 'res100.txt'
 #    filename3 = 'res1000.txt'
 
@@ -43,7 +43,7 @@ ax1 = fig.add_subplot(111)
 
 p11, = ax1.plot(data1[:,0],data1[:,2])
 p12, = ax1.plot(data1[:,0],data1[:,3])
-p13, = ax1.plot(data1[:,0],data1[:,4])
+p13, = ax1.plot(data1[:,0],data1[:,8])
 #p2, = ax1.plot(data2[:,0],data2[:,1], color='k', linestyle='-.', linewidth=3)
 #p31, = ax2.plot(data1[:,0],data1[:,3], linestyle='--')
 #p32, = ax2.plot(data2[:,0],data2[:,3], linestyle='--') 
@@ -53,12 +53,12 @@ ax1.set_ylabel('Wave function', fontsize=14)
 #ax2.set_ylabel(r'\log_{10}$(Rel. error)', fontsize=14)
 #ax1.set_ylim([0, 2])
 #ax2.set_ylim([-3, 5])
-#ax1.grid()
+#ax1grid()
 #ax2.grid()
 plt.legend([p11,p12,p13], \
-        [('n=1'), \
-        ('n=2' ), \
-        ('n=15' ) ] )
+        [('n=2'), \
+        ('n=3' ), \
+        ('n=8' ) ] )
 #        ('Analytical solution'), \
 #        ('Rel. error, N=%g points' % (N1,)),\
 #        ('Rel. error, N=%g points' % (N2,)),\
