138f856e7f5db71c4b440ef72f65002aa8056685
Marius Berge Eide <mariusbe@gmail.com>
2013-10-13 22:49:12 +0200

    Added RK4 reference

--- oblig3/headers.h
+++ oblig3/headers.h
@@ -5,11 +5,38 @@ void outputFile(int N,int cols,double *fX,double *fCalcX, char *filename[]);
 
 #endif
 
+#ifndef asystem_H
+#define asystem_H
+
+arma::vec aSystem(arma::vec Xi, arma::vec Xii);
+
+#endif
+
+
+
+#ifndef fvel_H
+#define fvel_H
+
+inline double fVel(double fMass, double fRadius);
+
+#endif
+
 
 #ifndef solver_EC_H
 #define solver_EC_H
 
 void solver_EC(   arma::vec (*vFuncIn)(arma::vec, arma::vec) \
+                , arma::mat *X \
+                , int N \
+                , int M \
+                , double dStep); 
+
+#endif
+
+#ifndef solver_RK4_H
+#define solver_RK4_H
+
+void solver_RK4(   arma::vec (*vFuncIn)(arma::vec, arma::vec) \
                 , arma::mat *X \
                 , int N \
                 , int M \
