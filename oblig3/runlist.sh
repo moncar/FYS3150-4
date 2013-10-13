679ebce91accf422e1c22450e294157fb794af9a
Marius Berge Eide <mariusbe@gmail.com>
2013-10-13 22:49:34 +0200

    Compiles and plots

---
+++ oblig3/runlist.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Check if everything has been compiled
+make
+
+# Fetch filename from argv
+if [ -z ${BASH_ARGV[0]} ]; then 
+    filen="res2"
+else 
+    filen=${BASH_ARGV[0]}
+fi
+
+echo $filen'.txt'
+
+# Storing runs in text files
+
+./obl3.x $filen'.txt'  
+#./obl2.x 200 0.5 16 $filen'2b.txt' $filen'2_vec.txt' &
+#./obl2.x 200 1 8 $filen'3b.txt' $filen'3_vec.txt' &
+#./obl2.x 200 5 4 $filen'4b.txt' $filen'4_vec.txt' &
+
+# Plot 
+python plotter.py $filen'.txt'
