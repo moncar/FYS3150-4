89d3b9d32bcb4892bc5d62b072999b53be535448
Marius Berge Eide <mariusbe@gmail.com>
2013-10-22 21:51:26 +0200

    Major changes, now working

--- oblig3/main.cpp
+++ oblig3/main.cpp
@@ -50,7 +50,7 @@ public:
     // Set up matrix to hold all velocities and positions
     {
         nIterations = N;
-        arma::mat A(5*nPlanets+1, N);
+        arma::mat A(5*nPlanets, N);
         // Data:
         // Columns: 0,1: xvel, yvel -> 2*nPlanets
         //          2,3: xpos, ypos -> 2*nPlanets
@@ -77,9 +77,10 @@ public:
     }
     
 
-    void SolveAll(arma::mat &Y, double dt) {
-        //solver_EC(&aSystem, &Y, nIterations, 5*nPlanets+1, dt);
-        solver_RK4(&aSystem, &Y, nIterations, 5*nPlanets+1, dt);
+    void SolveAll(arma::mat &Y, double dt, double dt0) {
+        //solver_EC(&aSystem, &Y, nIterations, 5*nPlanets, dt);
+        //solver_RK4(&aSystem, &Y, nIterations, 5*nPlanets, dt0, dt);
+        solver_DPRI(&aSystem, &Y, nIterations, 5*nPlanets, dt0, dt);
     }
 
 
@@ -89,74 +90,11 @@ inline double fVel(double fMass, double fRadius) {
     return sqrt(fMass/fRadius);
 }
 
-arma::vec aSystem(arma::vec Xi, arma::vec Xii) {
-    // Set up vector containing velocities, positions and time
-    // for entire system
-    // Pos: (x,y) -> 2 * nPlanets
-    // Vel: (x,y) -> 2 * nPlanets
-    // Mass:      -> 1 * nPlanets
-    // time          1
-    
-    //int nPlanets = (sizeof(Xi)/sizeof(double) - 1) / (5);
-    double nPlanets = ( Xi.size() - 1)/5;
-    vec Y(5*nPlanets+1);
-
-    //for planet k
-    //    interacting with planet j != k:
-    
-
-    for (int k=0; k<nPlanets; k++) {
-        //double fFtot = 0;   // Total force on each planet k
-        double fFtotx= 0;   //  x-component
-        double fFtoty= 0;   //  y-component
-        double fFloc = 0;   // Force on planet k from planet j
-        double fFlocx= 0;   //  x-component
-        double fFlocy= 0;   //  y-component
-        for (int j=0; j<nPlanets; j++) {
-            if (j != k) {
-                // Difference in position
-                double fDX = Xi[j*5+2] - Xi[k*5+2];
-                double fDY = Xi[j*5+3] - Xi[k*5+3];
-                double fRad= sqrt(fDX*fDX + fDY*fDY);
-
-                // Forces, divided by M[k]
-                fFloc = Xi[j*5+4] / (fRad * fRad);
-                fFlocx= fFloc * (Xi[j*5+2] - Xi[k*5+2])/fRad;  
-                fFlocy= fFloc * (Xi[j*5+3] - Xi[k*5+3])/fRad;
-                
-                fFtotx += fFlocx;
-                fFtoty += fFlocy;
-
-            }
-        }
-        // d2x / dt2
-        // Fx/M[k] = F/M[k] * cos theta = F/M[k] * x/r 
-        Y[k*5]      = fFtotx; 
-        
-        // d2y / dt2
-        // Fy/M[k] = F/M[k] * cos theta = F/M[k] * y/r 
-        //Y[k*5+1]    = fFtot * (Xi[j*5+3] - Xi[k*5+3])/fRad;
-        Y[k*5+1]    = fFtoty;
-
-        // dx  / dt
-        Y[k*5+2]    = Xii[k*5] ;
-        // dy  / dt
-        Y[k*5+3]    = Xii[k*5+1] ;
-
-        // dm / dt
-        Y[k*5+4]    = 0;
-    }
-
-    // dt / dt
-    Y[nPlanets*5] = 1.;
-
-    return Y;
-}
-
 int main(int argc, char* argv[]) {
     
-    int N=100000;
-    double dt = 1./3650;
+    int N=2400;
+    double dt = 1./356;
+    double t0 = 0.0;
 
     SolarSystem Sol;
     double fMsol = 332946.; // Earth masses
@@ -173,7 +111,7 @@ int main(int argc, char* argv[]) {
     // Earth
     Sol.AddPlanet(1./fMsol,     1.,     0.,     0.,  fVel(1., 1.));
     // Moon
-    //Sol.AddPlanet(0.0123/fMsol, 1.00257, 0., 0., fVel(1., 1.00257));
+    Sol.AddPlanet(0.0123/fMsol, 1.00257, 0., 0., 1.+ fVel(1./fMsol, 0.00257));
     // Mars
     Sol.AddPlanet(0.107/fMsol, -1.523679, 0., 0., fVel(1., 1.523679));
     // Jupiter
@@ -184,11 +122,13 @@ int main(int argc, char* argv[]) {
     Sol.AddPlanet(14.535/fMsol, 0., 19.229, -fVel(1., 19.229), 0.);
     // Neptune
     Sol.AddPlanet(17.147/fMsol, 0., -30.103, fVel(1., 30.103), 0.);
+    
+    
     mat A = Sol.ConstructArray(N);
 
     cout << "Class has # planets: " << Sol.PlanetCounter() << endl;
 
-    Sol.SolveAll(A, dt);
+    Sol.SolveAll(A, dt, t0);
 
     int nPlanets = Sol.PlanetCounter();    
 
@@ -201,13 +141,17 @@ int main(int argc, char* argv[]) {
     double* POS = new double[2*nPlanets*N];
     double* TIME= new double[N];
 
+    for (int iii=0; iii<N; iii++) {
+        TIME[iii] = t0 + iii*dt;
+    }
+
     for (int ppp=0; ppp<nPlanets; ppp++) {
         for (int kkk=0; kkk<N; kkk++) {
             int subscript = 2*ppp;
             POS[kkk +  subscript *N] = A(ppp*5+2,kkk);
             POS[kkk +  (subscript+1) *N] = A(ppp*5+3,kkk);
             
-            TIME[kkk]   = A(5*nPlanets,kkk);
+            //TIME[kkk]   = A(5*nPlanets,kkk);
         }
     }
 
