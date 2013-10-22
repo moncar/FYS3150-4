5285a127eee6c22e49edbad9007a75c36ae25d7f
Marius Berge Eide <mariusbe@gmail.com>
2013-10-22 22:28:27 +0200

    Major changes, effective

--- oblig3/main.cpp
+++ oblig3/main.cpp
@@ -3,6 +3,7 @@
 #include <armadillo>
 
 #include "headers.h"
+#include "SolarSystem.hpp"
 
 using namespace std;
 using namespace arma;
@@ -15,76 +16,7 @@ template <typename T> int sgn(T val) {
             return (T(0) < val) - (val < T(0));
 }
 
-class SolarSystem
-{
-private:
-    // Initial values have to be stored in a list
-    vector<double> M ;
-    vector<double> vx;
-    vector<double> vy;
-    vector<double> x ;
-    vector<double> y ;
 
-    int nPlanets = 0;   // will change as heavenly objects are added
-    int nIterations=0;  // to be defined by ConstructArray
-
-public:
-    void AddPlanet(double mass=1, double xpos=1, double ypos=0 \
-        ,double xvel=0, double yvel=0)
-    {
-        nPlanets += 1;
-        M.push_back(mass);
-        vx.push_back(xvel);
-        vy.push_back(yvel);
-        x.push_back(xpos);
-        y.push_back(ypos);
-        
-    }
-
-    int PlanetCounter() {
-        int nPlanetsPublic = nPlanets;
-        return nPlanetsPublic;
-    }
-
-    arma::mat ConstructArray(int N)
-    // Set up matrix to hold all velocities and positions
-    {
-        nIterations = N;
-        arma::mat A(5*nPlanets, N);
-        // Data:
-        // Columns: 0,1: xvel, yvel -> 2*nPlanets
-        //          2,3: xpos, ypos -> 2*nPlanets
-        //            4: time       -> 1
-        //
-        // Each planet is assigned an index k
-        
-        // Fill in: initial values
-
-        for (int k=0; k<nPlanets; k++) {
-            A(k*5  ,0) = vx[k];
-            A(k*5+1,0) = vy[k];
-            A(k*5+2,0) =  x[k];
-            A(k*5+3,0) =  y[k];
-            A(k*5+4,0) =  M[k];
-        }
-        A(nPlanets, 0) =    0;  // init. time
-
-        return A;
-    }
-
-    inline double Radius(double x, double y) {
-        return sqrt(x*x + y*y);
-    }
-    
-
-    void SolveAll(arma::mat &Y, double dt, double dt0) {
-        //solver_EC(&aSystem, &Y, nIterations, 5*nPlanets, dt);
-        //solver_RK4(&aSystem, &Y, nIterations, 5*nPlanets, dt0, dt);
-        solver_DPRI(&aSystem, &Y, nIterations, 5*nPlanets, dt0, dt);
-    }
-
-
-};
 
 inline double fVel(double fMass, double fRadius) {
     return sqrt(fMass/fRadius);
