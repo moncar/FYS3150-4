6707fb5ff56043446b475e5644fa97ab0099ccc2
Marius Berge Eide <mariusbe@gmail.com>
2013-10-29 00:00:18 +0100

    Cosmetic changes

--- oblig3/solver_RK4.cpp
+++ oblig3/solver_RK4.cpp
@@ -27,22 +27,18 @@ void solver_RK4(   arma::vec (*vFuncIn)(double, arma::vec) \
 
     for (int iii=0; iii<N-1; iii++) {
         // Iterate until the end of the array
-        //for (int j=0; j<M; j++) {
-            // Iterate through each equation
-            // Recalculate each of them as dStep could change (later)
             
-            k1 = (*vFuncIn)(ti, X->col(iii));
-            k2 = (*vFuncIn)(ti + 0.5*dStep,\
-                   X->col(iii) + k1*0.5*dStep) ;
-            k3 = (*vFuncIn)(ti + 0.5*dStep,\
-                   X->col(iii) + k2*0.5*dStep) ;
-            k4 = (*vFuncIn)(ti + dStep, \
-                   X->col(iii) + k3*dStep) ;
+        k1 = (*vFuncIn)(ti, X->col(iii));
+        k2 = (*vFuncIn)(ti + 0.5*dStep,\
+               X->col(iii) + k1*0.5*dStep) ;
+        k3 = (*vFuncIn)(ti + 0.5*dStep,\
+               X->col(iii) + k2*0.5*dStep) ;
+        k4 = (*vFuncIn)(ti + dStep, \
+               X->col(iii) + k3*dStep) ;
 
-            X->col(iii+1) = X->col(iii) + \
-                          1./6 * dStep * (k1 + 2*k2 + 2*k3 + k4);
+        X->col(iii+1) = X->col(iii) + \
+                      1./6 * dStep * (k1 + 2*k2 + 2*k3 + k4);
 
-        //}
         ti += dStep;
     }
 
