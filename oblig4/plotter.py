254faf70d0e8eab1f03c7a5156b3d9aebb644319
Marius Berge Eide <mariusbe@gmail.com>
2013-11-11 22:56:41 +0100

    Plot errors and solutions

--- oblig4/plotter.py
+++ oblig4/plotter.py
@@ -8,7 +8,7 @@ import sys
 # Data from files
 try:
     filename1 = sys.argv[1]
-#    filename2 = sys.argv[2]
+    filename2 = sys.argv[2]
 #    filename3 = sys.argv[3]
 #    filename4 = sys.argv[4]
 except IndexError:
@@ -18,17 +18,17 @@ except IndexError:
 #    filename4 = 'resfinal4_vec.txt'
 
 data1 = loadtxt(filename1);
-#data2 = loadtxt(filename2);
+data2 = loadtxt(filename2);
 #data3 = loadtxt(filename3);
 #data4 = loadtxt(filename4);
 
 # Find no. of iterations
-N1 = len(data1[:,0])
+N1 = len(data1[0,:])
 #N2 = len(data2[:,0])
 #N3 = len(data3[:,0])
 
 # Find no. of planets
-nK = len(data1[0,:] - 1)/2
+nK = len(data1[:,0]-1)
 
 print "Length of arrays to plot: ", N1
 print "Number of planets to plot: ", nK
@@ -48,27 +48,39 @@ else:
 
 
 fig = plt.figure()
+figE= plt.figure()
+
 plt.rc('text', usetex=True)
 plt.rc('font', family='serif')
 #plt.rc({'xtick.labelsize': 20})
 #plt.rc({'ytick.labelsize': 20})
 
 ax1 = fig.add_subplot(111)
+axE = figE.add_subplot(111)
 #ax2 = ax1.twinx()
 ax1labels = []
 linestyles = ['k-', 'y-', 'b-', 'kx', 'r-', 'y-', 'c-', 'b-', 'g-']
 
-for i in xrange(0,nK-1):
-    # Plot planets, last item is Sun
-    ax1.plot(data1[::slice,2*i+1], data1[::slice, 2*i+2], linestyles[i])
+for i in xrange(2, nK-1,  (nK-1)/((nK-1)/42)):
+    ax1.plot(data1[0,:], data1[i,:]+1-data2[0,:])
+    ax1labels.append('t = %.3g' % (i*1./(nK-1)))
+    #ax1.plot(data1[2*i+1,::slice], data1[2*i+2,::slice], linestyles[i])
 
-p19, = ax1.plot(data1[::slice,2*nK-1],data1[::slice,2*nK], 'y-x')  # SUN
+
+# # Plot errors
+for i in xrange(2, nK-1,  (nK-1)/((nK-1)/42)):
+    err = 1 - abs(  (data1[i,:] + (1 - data2[0,:]) )/(1 - data2[0,:]) )
+    axE.plot(data1[0,:], err) 
 
 
-ax1.set_xlabel(r'Rel. $x$-position ' + r'$\chi_x$', fontsize=14)
-ax1.set_ylabel(r'Rel. $y$-position ' + r'$\chi_y$', fontsize=14)
+ax1.set_xlabel(r'$x$-position ', fontsize=14)
+ax1.set_ylabel(r'$u(x,t) = v(x,t) - u_s (x)$ ', fontsize=14)
+axE.set_xlabel(r'$x$-position ', fontsize=14)
+axE.set_ylabel(r'Error $ 1 - |u(x,t) / - u_s (x) |$ ', fontsize=14)
 #ax1.set_ylim([-4, 4])
 #ax1.set_xlim([-4, 4])
+
+
 
 #ax2.set_ylim([-3, 5])
 #ax1grid()
@@ -82,4 +94,10 @@ ax1.set_ylabel(r'Rel. $y$-position ' + r'$\chi_y$', fontsize=14)
 #        ('Rel. error, N=%g points' % (N1,)),\
 #        ('Rel. error, N=%g points' % (N2,)),\
 #        ('Rel. error, N=%g points' % (N3,))] )
+
+box = ax1.get_position()
+ax1.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+ax1.legend(ax1labels, loc='center left', bbox_to_anchor=(1, 0.5))
+axE.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+axE.legend(ax1labels, loc='center left', bbox_to_anchor=(1, 0.5))
 plt.show()
