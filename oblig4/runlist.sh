cb51ed6412c491e915f60faeef919228ee8f4c29
Marius Berge Eide <mariusbe@gmail.com>
2013-11-11 22:57:00 +0100

    Just run

--- oblig4/runlist.sh
+++ oblig4/runlist.sh
@@ -4,24 +4,24 @@
 make
 
 # Fetch filename from argv
-if [ -z ${BASH_ARGV[0]} ]; then 
-    filen_pos="res2"
-    filen_vel="res2vel"
-    filen_energy="res2energy"
-else 
-    filen_pos=${BASH_ARGV[0]}
-    filen_vel=${BASH_ARGV[1]}
-    filen_energy=${BASH_ARGV[2]}
-fi
+#if [ -z ${BASH_ARGV[0]} ]; then 
+#    filen_pos="res2"
+#    filen_vel="res2vel"
+#    filen_energy="res2energy"
+#else 
+#    filen_pos=${BASH_ARGV[0]}
+#    filen_vel=${BASH_ARGV[1]}
+#    filen_energy=${BASH_ARGV[2]}
+#fi
 
-echo $filen_pos'.txt'
-echo $filen_vel'.txt'
-echo $filen_energy'.txt'
+#echo $filen_pos'.txt'
+#echo $filen_vel'.txt'
+#echo $filen_energy'.txt'
 
 # Storing runs in text files
 
-./obl3.x $filen_pos'.txt' $filen_vel'.txt' $filen_energy'.txt'
+./obl4.x resFE.txt resBE.txt resCN.txt resXSTEPS.txt
 
 # Plot 
-echo "Data has now been sent to plotter. Plotting..."
-python plotter.py $filen_pos'.txt'
+#echo "Data has now been sent to plotter. Plotting..."
+#python plotter.py $filen_pos'.txt'
