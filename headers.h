9b232d335f8f0558c6aebcb9fde6c36288e67520
Marius Berge Eide <mariusbe@gmail.com>
2013-12-02 19:10:41 +0100

    Headers

---
+++ headers.h
@@ -0,0 +1,9 @@
+#ifndef outputFile_H
+#define outputFile_H
+void outputFile(int N,int cols,double *fX,double *fCalcX, char *filename[]);
+#endif
+
+#ifndef outputFile2_H
+#define outputFile2_H
+void outputFile2(int N,int rows,double *fX,double *fCalcX, char *filename[]);
+#endif
