b41012929a8a654b029397c4aff24235a95590cf
Marius Berge Eide <mariusbe@gmail.com>
2013-12-07 01:16:45 +0100

    Reflecting changes in wave function

--- oblig5/mainTESTSUITE.cpp
+++ oblig5/mainTESTSUITE.cpp
@@ -36,6 +36,7 @@ int mainTESTSUITE(int argc, char* argv[], int N) {
 
     double xstep = 0.5;
     double alpha = 1.0;
+    double beta  = 2.0;
 
     double* locenergy;
 
@@ -48,6 +49,7 @@ int mainTESTSUITE(int argc, char* argv[], int N) {
                         ,xstep \
                         , N \
                         , alpha \
+                        , beta \
                         , locenergy);
 
     cout << "Energy " << locenergy[0] << endl;
